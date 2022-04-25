52a61c5cd322d7be46338d0b4502cc6627a133ac
Ulugbek Salaev <ulugbek0302@gmail.com>
2022-04-25 09:38:31 +0500

    Initial comment

--- src/UzTranslit.py
+++ src/UzTranslit.py
@@ -1,9 +1,11 @@
 import data
+
+
 class UzTranslit:
-    __cmap= {}
+    __cmap = {}
 
     def __init__(self):
-        #self.__initial_data()
+        # self.__initial_data()
         __data = data.Mapping()
         self.__cmap['cyr_lat'] = __data.cyr_lat
         self.__cmap['cyr_nlt'] = __data.cyr_nlt
@@ -14,27 +16,44 @@ class UzTranslit:
 
     def __initial_data(self):
         return ""
-        #print(d.cyr_lat)
-        #print(d.cyr_lat['a'])
-        #print(d.cyr_lat['b'])
-        #print( isinstance(d.cyr_lat['a'], list))
+        # print(d.cyr_lat)
+        # print(d.cyr_lat['a'])
+        # print(d.cyr_lat['b'])
+        # print( isinstance(d.cyr_lat['a'], list))
 
-        #people = {'cyrlat': {'a': 'аа', 'б': 'b', '#': '-'},
+        # people = {'cyrlat': {'a': 'аа', 'б': 'b', '#': '-'},
         #          'lat': {'a': 'аа', 'б': 'аа', 'sex': 'Female'}}
-        #print(people['cyr'+'lat']['#'])
+        # print(people['cyr'+'lat']['#'])
 
-    def translit(self, text, from_='cyr', to='lat'):
-        sc = self.__cmap[from_+'_'+to] #selected script mapping
-        print(sc)
+    def translit(self, text, from_: str = 'cyr', to: str ='lat'):
+        sc_map = self.__cmap[from_ + '_' + to]  # selected script mapping
+        print(sc_map)
         N = 4
-        words = text.split()
+        words = text.split()    # list of words from text
+        cnv_words = []          # list of converted words
         for word in words:
+            # print(word)
             grams = [word[i:i + N] for i in range(len(word) - N + 1)]
-            print(grams)
+            # print(grams)
 
-            #words_cnv =
-
-
+            cnv_word = ""   # converted version of the current word
+            i = 0
+            wl = len(word)
+            while i < wl:
+                found = False
+                for j in range(wl - i, 0, -1):
+                    chunk = word[i: i + j]
+                    #print(chunk)
+                    if chunk in sc_map:
+                        cnv_word += sc_map[chunk]
+                        found = True
+                        i += j
+                        break
+                if not found:
+                    cnv_word += word[i]
+                    i += 1
+            cnv_words.append(cnv_word)
+        return ' '.join(cnv_words)    # return as a list // return cnv_words
 
 obj = UzTranslit()
-obj.translit("Салом хуш келибсиз!")
+print(obj.translit("Салом хуш кел, бахор! Bizlarni maktabimiz yoradigan xili#$$", 'lat', 'cyr'))
