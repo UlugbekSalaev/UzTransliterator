15d6257d521f41b5dbbcf0df483542e802e9b53e
ulugbek0302 <ulugbek0302@gmail.com>
2022-09-26 13:02:23 +0500

    fixed

--- src/UzTransliterator/UzTransliterator.py
+++ src/UzTransliterator/UzTransliterator.py
@@ -602,30 +602,40 @@ class UzTransliterator:
                 self.__lat_exwords[x[0]] = x[1]
 
     def __cyr_rule1(self, word: str, i: int):  # 2ta undosh orasida kelgan е harfi e harfi shaklida buladi
+        word, i = self.__remove_symbols_starting(word, i)
+
         e_map1 = {'е': 'e', 'Е': 'E'}
         e_map2 = {'е': 'ye', 'Е': 'Ye'}
         if i == len(word) - 1:  # last char alifbe->алифбэ->алифбе
             return e_map1[word[i]]
 
         if i - 1 >= 0 and i + 1 < len(word):
-            if word[i - 1] not in self.__cyr_vowel and word[i + 1] not in self.__cyr_vowel and word[i - 1] not in ['(','"','-', '-', '.', ',', '?','!']:
+            if word[i - 1] not in self.__cyr_vowel and word[i + 1] not in self.__cyr_vowel and \
+                    word[i - 1] not in ['(', '"', '-', '-', '.', ',', '?', '!']:
                 return e_map1[word[i]]
 
         if i - 1 >= 0:  # археолог->arxyeolog xatoni yuqatish uchun
-            if word[i - 1] not in self.__cyr_vowel and word[i - 1] not in ['(','"','-', '-', '.', ',', '?','!']:
+            if word[i - 1] not in self.__cyr_vowel and word[i - 1] not in ['(', '"', '-', '-', '.', ',', '?', '!']:
                 return e_map1[word[i]]
 
         return e_map2[word[i]]
 
     def __cyr_rule2(self, word: str, i: int):  # 2ta unli orasida kelgan ц harfi ts harfi shaklida o'giriladi
+        word, i = self.__remove_symbols_starting(word, i)
+
         s_map1 = {'ц': 'ts', 'Ц': 'TS'}
         s_map2 = {'ц': 's', 'Ц': 'S'}
         if i - 1 >= 0 and i + 1 < len(word):
-            if word[i - 1] in self.__cyr_vowel and word[i + 1] in self.__cyr_vowel and word[i - 1] not in ['(','"','-', '-', '.', ',', '?','!']:
+            if word[i - 1] in self.__cyr_vowel and word[i + 1] in self.__cyr_vowel and word[i - 1] not in ['(', '"',
+                                                                                                           '-', '-',
+                                                                                                           '.', ',',
+                                                                                                           '?', '!']:
                 return s_map1[word[i]]
         return s_map2[word[i]]
 
     def __cyr_rule3(self, word: str, i: int):  # [е,ё,ю,я] harflari undosh tovushlardan keyin kelganda [e,o,u,a] shaklida o'giriladi, masalan sentabr, istisno holatlar mavjud:samalyot
+        word, i = self.__remove_symbols_starting(word, i)
+
         y_map1 = {
             'е': 'е', 'Е': 'E',
             'ё': 'o', 'Ё': 'O',
@@ -643,19 +653,16 @@ class UzTransliterator:
             return y_map2[word[i]]
 
         if i - 1 >= 0 and i != len(word) - 1:
-            if word[i - 1] not in self.__cyr_vowel and word[i - 1] not in ['(','"','-', '-', '.', ',', '?','!']:  # bulardan oldingi harf undosh bulsa
+            if word[i - 1] not in self.__cyr_vowel and word[i - 1] not in ['(', '"', '-', '-', '.', ',', '?',
+                                                                           '!']:  # bulardan oldingi harf undosh bulsa
                 return y_map1[word[i]]
         return y_map2[word[i]]
 
     def __lat_rule1(self, word: str, i: int):  # latin -> kiril # 2ta undosh orasida kelgan е harfi e harfi shaklida buladi
+        word, i = self.__remove_symbols_starting(word, i)
+
         e_map1 = {'e': 'е', 'E': 'Е'}  # latin -> kiril
         e_map2 = {'e': 'э', 'E': 'Э'}  # latin -> kiril
-
-        ni = i  # temp variable, delete starting symbols to correctly convert of (endi  -> (энди
-        for ind in range(ni):
-            if not word[ind].isalpha():
-                i -= 1
-                word = word[1:]
 
         if i - 1 >= 0 and i + 1 < len(word):
             if word[i - 1] not in self.__lat_vowel and word[i + 1] not in self.__lat_vowel:
@@ -667,13 +674,28 @@ class UzTransliterator:
 
         return e_map2[word[i]]
 
-    def __check_change_date(self, cnv_words: list):  # kirill->latin da date larga chiziqcha quyish
+    def __check_change_date_to_lat(self, cnv_words: list):  # kirill->latin da date larga chiziqcha quyish
         dates = (
             'yil', 'asr', 'yanvar', 'fevral', 'mart', 'aprel', 'may', 'iyun', 'iyul', 'avgust', 'sentabr', 'oktabr',
             'noyabr', 'dekabr')
         for i in range(1, len(cnv_words)):
             if cnv_words[i].lower().startswith(dates) and cnv_words[i - 1].isdigit():
                 cnv_words[i] = "\*-" + cnv_words[i]  # "\*-" quyib keyin " \*" ni o'chirib tashaymiz
+        # return cnv_words       #list is a mutable object, so it is sent as a refrence to the object
+
+    def __check_change_date_to_cyr(self, cnv_words: list):  # latin->cyril da date larga chiziqcni quymaslik
+        dates = (
+            'йил', 'аср', 'январ', 'феврал', 'март', 'апрел', 'май', 'июн', 'июл', 'август', 'сентябр', 'октябр',
+            'ноябр', 'декабр')
+
+        for i in range(len(cnv_words)):
+            if "-" in cnv_words[i]:
+                # remove starting symbols
+                ni = cnv_words[i].find("-")
+                if ni > 0 and ni + 1 < len(cnv_words[i]):
+                    if cnv_words[i][ni - 1].isdigit():
+                        if cnv_words[i].lower().startswith(dates, ni + 1):
+                            cnv_words[i] = cnv_words[i][:ni] + " " + cnv_words[i][ni + 1:]  # chiziqchani uchirbaramiz
         # return cnv_words       #list is a mutable object, so it is sent as a refrence to the object
 
     def __check_change_second_uppercase(self, cnv_words: list):  # SHamol ->Shamol
@@ -686,11 +708,29 @@ class UzTransliterator:
                 else:
                     break
 
-            if cnv_words[i][ind:ind+2] in twoletters and cnv_words[i][ind + 2:].islower():
-                cnv_words[i] = cnv_words[i][:ind] + twoletters[cnv_words[i][ind:ind+2]] + cnv_words[i][ind+2:]  # SHamol ->Shamol
+            if cnv_words[i][ind:ind + 2] in twoletters and cnv_words[i][ind + 2:].islower():
+                cnv_words[i] = cnv_words[i][:ind] + twoletters[cnv_words[i][ind:ind + 2]] + cnv_words[i][
+                                                                                            ind + 2:]  # SHamol ->Shamol
         # return cnv_words
 
-    def check_global_rules(self, word: str):
+    def __remove_symbols_starting(self, word: str, i: int):  # delete starting symbols to correctly convert of (endi  -> (энди
+        ni = i  # temp variable
+        for ind in range(ni):
+            if not word[ind].isalpha():
+                i -= 1
+                word = word[1:]
+            else:
+                break
+        for ind in range(len(word)-1, i,  -1):
+            if not word[ind].isalpha():
+                word = word[:-1]
+            else:
+                break
+
+        return word, i
+        # edn of deleting symbols from start of the word
+
+    def __check_global_rules(self, word: str):
         # global rule 1: whether Roman number
         if word.isupper():  # check whether token is Roman number {I, V, X, L, C, D, M}
             if 0 not in [c in {'I', 'V', 'X', 'L', 'C', 'D', 'M'} for c in word]:
@@ -737,7 +777,7 @@ class UzTransliterator:
         for word in tokens:
             cnv_word = ""  # converted version of the current word
 
-            if self.check_global_rules(word):
+            if self.__check_global_rules(word):
                 cnv_words.append(word)  # add itself to converted words list
                 continue  # skip converted process if current word is Roman number
 
@@ -748,7 +788,7 @@ class UzTransliterator:
                 search_ex = True
                 for j in range(wl - i, 0, -1):
                     chunk = word[i: i + j]
-                    # print("chunk="+chunk)
+                    # print(i, j, "chunk=", chunk)
 
                     # latin->kirilda character_mapping qilmasdan oldin, ushbu suzni exwords dan qidirib, topilsa shunga o'giramiz
                     if to == "cyr" and search_ex:  # i==0 bu suzni boshidagi qismini csv dan qidirish
@@ -762,8 +802,8 @@ class UzTransliterator:
                             found = True
                             i += j
                             break
-                        if chunk[0].isalpha():
-                            search_ex = False
+                        # if chunk[0].isalpha() and chunk[-1].isalpha():
+                        #     search_ex = False
                     if to in ["lat", "nlt"] and search_ex:  # i==0 bu suzni boshidagi qismini csv dan qidirish
                         if chunk.lower() in self.__lat_exwords:
                             res = self.__lat_exwords[chunk.lower()]
@@ -774,8 +814,8 @@ class UzTransliterator:
                             found = True
                             i += j
                             break
-                        if chunk[0].isalpha():
-                            search_ex = False
+                        # if chunk[0].isalpha() and chunk[-1].isalpha():
+                        #     search_ex = False
 
                     if chunk in sc_map:
                         cnv_word += sc_map[chunk]
@@ -808,9 +848,15 @@ class UzTransliterator:
 
             cnv_words.append(cnv_word)
 
+        if to in ["cyr"]:
+            self.__check_change_date_to_cyr(
+                cnv_words)  # latin->cyrill o'girilganda sanalar oldiga chiziq o'chiriladi: 2021-yil 10-mart -> 2021 йил 10 март
+
         if to in ["lat", "nlt"]:
-            self.__check_change_date(cnv_words)  # kiril->latin o'girilganda sanalar oldiga chiziqcha qo'yiladi: 2021 йил 10 март -> 2021-yil 10-mart
-            self.__check_change_second_uppercase(cnv_words)  # latin va newLatinda Birinchidagi harf katta bulsa SHamol shaklida qaytganda Shamol qilib yuborish uchun
+            self.__check_change_date_to_lat(
+                cnv_words)  # kiril->latin o'girilganda sanalar oldiga chiziqcha qo'yiladi: 2021 йил 10 март -> 2021-yil 10-mart
+            self.__check_change_second_uppercase(
+                cnv_words)  # latin va newLatinda Birinchidagi harf katta bulsa SHamol shaklida qaytganda Shamol qilib yuborish uchun
 
         text = ' '.join(cnv_words)  # return as a list // return cnv_words
 
@@ -818,39 +864,3 @@ class UzTransliterator:
             text = text.replace(" \*-", "-")  # date dagi uzgarihslar uchun
 
         return text
-
-
-obj = UzTransliterator()
-
-while True:
-    lang1 = input("lang1=")
-    lang2 = input("lang2=")
-    w = ""
-    while w != "stop":
-        w = input('Suz=')
-        print(obj.transliterate(w, from_=lang1, to=lang2))
-
-#qoida: lotin->kiril e oxorda kelsa e buladi, Э емас alifbe
-for i in range(1):
-    from_ = "lat"
-    to = "cyr"
-    with open(os.path.dirname(__file__)+'/../../test/'+str(i)+'_'+from_+'.txt', encoding='utf8') as f:
-        text = f.read().rstrip()
-        tokens = text.split()
-        print(len(text))
-        print(len(tokens))
-
-    with open(os.path.dirname(__file__)+'/../../test/'+str(i)+'_'+to+'.txt', encoding='utf8') as f:
-        text_to = f.read().rstrip()
-        tokens_to = text_to.split()
-        print(len(text_to))
-        print(len(tokens_to))
-        cnt, total = 0, 0
-    for ind in range(len(tokens)):
-        res = obj.transliterate(tokens[ind], from_=from_, to=to)
-        if res not in tokens_to:
-            print(tokens[ind], res)
-            cnt += 1
-        total += 1
-    print(total, cnt, cnt/total*100)
-
---
+++ src/UzTransliterator/test.py
@@ -0,0 +1,48 @@
+import UzTransliterator
+import os
+import time
+
+obj = UzTransliterator.UzTransliterator()
+
+# while True:
+#     lang1 = input("lang1=")
+#     lang2 = input("lang2=")
+#     w = ""
+#     while w != "stop":
+#         w = input('Suz=')
+#         print(obj.transliterate(w, from_=lang1, to=lang2))
+total_token, total_char, total_time = 0, 0, 0
+for i in range(65):
+    from_ = "lat"
+    to = "cyr"
+    with open(os.path.dirname(__file__)+'/../../test/'+str(i)+'_'+from_+'.txt', encoding='utf8') as f:
+        text = f.read().rstrip()
+        tokens = text.split()
+        # print(len(text))
+        # print(len(tokens))
+        total_token += len(tokens)
+        total_char += len(text)
+
+    with open(os.path.dirname(__file__)+'/../../test/'+str(i)+'_'+to+'.txt', encoding='utf8') as f:
+        text_to = f.read().rstrip()
+        tokens_to = text_to.split()
+        # print(len(text_to))
+        # print(len(tokens_to))
+
+    start_time = time.time()
+    result = obj.transliterate(text, from_=from_, to=to)
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    total_time += time.time() - start_time
+    tokens_result = result.split()
+    wrong, correct = 0, 0
+    for token in tokens_result:
+        if token not in tokens_to:
+            # print(token)
+            wrong += 1
+        else:
+            tokens_to.remove(token)
+            correct += 1
+
+    print(i, wrong, correct, len(tokens_result), wrong/len(tokens_result)*100, correct/len(tokens_result)*100)
+
+
