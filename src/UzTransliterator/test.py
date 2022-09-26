3ad5b390890f45cccfd665e1e38c3d2b6cc38b74
ulugbek0302 <ulugbek0302@gmail.com>
2022-09-26 13:17:28 +0500

    fixed

--- src/UzTransliterator/test.py
+++ src/UzTransliterator/test.py
@@ -12,7 +12,7 @@ obj = UzTransliterator.UzTransliterator()
 #         w = input('Suz=')
 #         print(obj.transliterate(w, from_=lang1, to=lang2))
 total_token, total_char, total_time = 0, 0, 0
-for i in range(65):
+for i in range(20,21):
     from_ = "lat"
     to = "cyr"
     with open(os.path.dirname(__file__)+'/../../test/'+str(i)+'_'+from_+'.txt', encoding='utf8') as f:
@@ -37,12 +37,14 @@ for i in range(65):
     wrong, correct = 0, 0
     for token in tokens_result:
         if token not in tokens_to:
-            # print(token)
+            print(token)
             wrong += 1
         else:
             tokens_to.remove(token)
             correct += 1
 
-    print(i, wrong, correct, len(tokens_result), wrong/len(tokens_result)*100, correct/len(tokens_result)*100)
-
+    print(i, len(result), wrong, correct, len(tokens_result), round(wrong/len(tokens_result)*100,2), round(correct/len(tokens_result)*100,2))
+print(total_time)
+print(total_token)
+print(total_char)
 
